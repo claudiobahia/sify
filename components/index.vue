69813f0dbbfbb80620d2dd71799446cb58e5738b
claudiobahia <claudio.bahia@al.infnet.edu.br>
2020-02-07 09:18:41 -0300

    atualizar, falta cub, quadrado, raiz quad e cub

---
+++ components/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="components">
+
+    </div>
+</template>
+
+<style lang="sass" scoped>
+    @import './index.scss';
+</style>
+
+<script>
+
+
+export default {
+    name: 'Components',
+    data() {
+        return {
+            
+        };
+    },
+    
+    methods: {
+        
+    },
+    mounted() {
+        
+    },
+};
+</script>
---
+++ src/components/mamamia/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="mamamia">
+
+    </div>
+</template>
+
+<style lang="sass" scoped>
+    @import './index.scss';
+</style>
+
+<script>
+
+
+export default {
+    name: 'Mamamia',
+    data() {
+        return {
+            
+        };
+    },
+    
+    methods: {
+        
+    },
+    mounted() {
+        
+    },
+};
+</script>
